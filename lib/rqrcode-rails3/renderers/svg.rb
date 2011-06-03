daea9ecefdfdc4ccf8e7e71907cf685f81214877
Ezra Spier <ezra.spier@gmail.com>
2011-06-03 12:18:43 -0700

    Added module_dimension option to the SVG renderer, to allow the user to specify the size of the rendered SVG object.

--- lib/rqrcode-rails3/renderers/svg.rb
+++ lib/rqrcode-rails3/renderers/svg.rb
@@ -4,16 +4,18 @@ module RQRCode
       class << self
         # Render the SVG from the qrcode string provided from the RQRCode gem
         #   Options:
-        #   offset - Padding around the QR Code (e.g. 10)
-        #   fill   - Background color (e.g "ffffff" or :white)
-        #   color  - Foreground color for the code (e.g. "000000" or :black)
+        #   offset            - Padding around the QR Code (e.g. 10)
+        #   fill              - Background color (e.g "ffffff" or :white)
+        #   color             - Foreground color for the code (e.g. "000000" or :black)
+        #   module_dimension  - The dimension of each module in the QR code in pixels (e.g. 11)
 
         def render(qrcode, options={})
-          offset  = options[:offset].to_i || 0
-          color   = options[:color]       || "000"
+          offset           = options[:offset].to_i      || 0
+          color            = options[:color]            || "000"
+          module_dimension = options[:module_dimension] || 11
 
           # height and width dependent on offset and QR complexity
-          dimension = (qrcode.module_count*11) + (2*offset)
+          dimension = (qrcode.module_count*module_dimension) + (2*offset)
 
           xml_tag   = %{<?xml version="1.0" standalone="yes"?>}
           open_tag  = %{<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ev="http://www.w3.org/2001/xml-events" width="#{dimension}" height="#{dimension}">}
@@ -23,11 +25,11 @@ module RQRCode
           qrcode.modules.each_index do |c|
             tmp = []
             qrcode.modules.each_index do |r|
-              y = c*11 + offset
-              x = r*11 + offset
+              y = c*module_dimension + offset
+              x = r*module_dimension + offset
 
               next unless qrcode.is_dark(c, r)
-              tmp << %{<rect width="11" height="11" x="#{x}" y="#{y}" style="fill:##{color}"/>}
+              tmp << %{<rect width="#{module_dimension}" height="#{module_dimension}" x="#{x}" y="#{y}" style="fill:##{color}"/>}
             end 
             result << tmp.join
           end
@@ -42,3 +44,4 @@ module RQRCode
     end
   end
 end
+
